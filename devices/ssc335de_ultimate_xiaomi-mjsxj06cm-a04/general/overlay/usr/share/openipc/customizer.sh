abd197246222625260bd832b052c63959ad80881
kirill9617 <kirill9617@yandex.ru>
2025-10-05 20:39:41 +0200

    basic hawdware configuration

--- devices/ssc335de_ultimate_xiaomi-mjsxj06cm-a04/general/overlay/usr/share/openipc/customizer.sh
+++ devices/ssc335de_ultimate_xiaomi-mjsxj06cm-a04/general/overlay/usr/share/openipc/customizer.sh
@@ -8,21 +8,34 @@
 fw_setenv upgrade 'https://github.com/kirill9617/builder/releases/download/latest/ssc335de_ultimate-xiaomi-mjsxj06cm-a04-nor.tgz'
 #
 #
+# Set sensor config
+fw_setenv sensor sc3338
+cli -s .isp.sensorConfig /etc/sensors/sc3338.bin
+
 # Set custom majestic settings
 #
+cli -s .system.plugins true
 #cli -s .video0.codec h264
-#cli -s .image.mirror false
-#cli -s .image.flip false
-#cli -s .nightMode.irCutPin1 79
-#cli -s .nightMode.irCutPin2 78
-#cli -s .nightMode.backlightPin 52
-#cli -s .nightMode.colorToGray true
-#cli -s .audio.inputChannel 1
-#cli -s .audio.outputEnabled true
-#cli -s .audio.outputVolume 80
-#cli -s .audio.speakerPin 15
-#cli -s .audio.speakerPinInvert true
-#cli -s .audio.srate 48000
+cli -s .image.mirror true
+cli -s .image.flip true
+
+cli -s .nightMode.irCutPin1 79
+cli -s .nightMode.irCutPin2 78
+cli -s .nightMode.backlightPin 52
+cli -s .nightMode.colorToGray true
+cli -s .nightMode.minThreshold 2000
+cli -s .nightMode.minThreshold 9000
+cli -s .nightMode.monitorDelay 0
+
+
+cli -s .audio.enabled true
+cli -s .audio.volume 60
+cli -s .audio.srate 48000
+
+cli -s .audio.outputEnabled true
+cli -s .audio.outputVolume 100
+cli -s .audio.speakerPin 63
+cli -s .audio.speakerPinInvert false
 #
 #
 # Set wlan device and credentials if need
